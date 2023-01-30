26127bead466b33395d140b4007ed07ff4b304c4
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-30 11:54:00 +0530

    asynchronous stream

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿class MyClass
+{
+    static async Task Main(string[] args)
+    {
+        var producer=new Producer();
+        //var numbers=await producer.GetNumbersAsync();
+        //foreach (var num in numbers)
+        //{
+        //    Console.WriteLine(num);
+        //}
+
+        await foreach(var num in producer.EnumerateNumbersAsync())
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
+public class Producer
+{
+    public async Task<IEnumerable<int>> GetNumbersAsync()
+    {
+        List<int> numbers = new List<int>();
+        for(int i=0; i<10; i++)
+        {
+            Console.WriteLine("Get some numbers");
+            for(int j=0; j<10; j++)
+            {
+                numbers.Add(i*10+j);
+            }
+            Console.WriteLine("Making next request....");
+            await Task.Delay(1500);
+        }
+        return numbers;
+    }
+
+    internal async IAsyncEnumerable<int> EnumerateNumbersAsync()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Get some numbers");
+            for (int j = 0; j < 10; j++)
+            {
+                yield return i*10+j;
+            }
+            Console.WriteLine("Making next request....");
+            await Task.Delay(1500);
+        }
+    }
+}
